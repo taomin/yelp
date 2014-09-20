eb7af76a54c3f99464daaf947ac458b5e2c68d3a
taomin <itiachang@gmail.com>
2014-09-20 13:25:56 -0700

    Initial Commit

---
+++ yelp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  yelp
+//
+//  Created by Taomin Chang on 9/20/14.
+//  Copyright (c) 2014 Taomin Chang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
